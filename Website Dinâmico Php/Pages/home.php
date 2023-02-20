0545b3be8343536d32a9fa1439463f58592e4a1c
unknown <121496415+Caio-dev00@users.noreply.github.com>
2023-02-19 22:29:18 -0300

    WebSite Dinâmico PHP

---
+++ Website Dinâmico Php/Pages/home.php
@@ -0,0 +1,87 @@
+	<section class="banner-container">
+		<div class="overlay"></div>
+		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/foto1.jpg');" class="banner-single"></div>
+		<div style="background-image: url('<?php echo INCLUDE_PATH; ?>images/background.jpg');" class="banner-single"></div>
+		<div class="center">
+		<form>
+			<h2>Qual seu melhor email?</h2>
+			<input type="email" name="email">
+			<input type="submit" name="acao" value="Cadastrar">
+		</form>
+	</div><!--Center-->
+	<div class="bullets">
+		
+	</div>
+	</section>
+
+	<section class="descricao-autor">
+		<div class="center">
+			<div class="w50 left">
+			<h2>Caio H. Marques</h2>
+			<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel orci non massa scelerisque mollis. Duis tincidunt hendrerit sodales. Integer ut ante eleifend, luctus purus quis, laoreet ante. Morbi blandit ligula augue, in aliquet augue vehicula a. Ut quis velit eleifend, interdum sapien eget, tristique mauris.</p>
+		</div><!--w50-->	
+		
+		<div class="w50 left">
+			<img class="right" src="<?php echo INCLUDE_PATH; ?>images/eu.jpg">	
+		</div><!--w50-->
+		<div class="clear"></div>
+		</div><!--Center-->	
+	</section><!--descricao-autor-->
+
+	<section class ="especialidades">
+		<div class="center">
+			<h2 class="title">Especialidades</h2>
+			<div class="w33 left box-especialidades">
+				<h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+				<h4>CSS3</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit.</p>
+			</div><!--box-especialidades-->
+				<div class="w33 left box-especialidades">
+				<h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+				<h4>HTML5</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit.</p>
+			</div><!--box-especialidades-->
+				<div class="w33 left box-especialidades">
+				<h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
+				<h4>JavaScript</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit.</p>
+			</div><!--box-especialidades-->
+			<div class="clear"></div>
+		</div><!--Center-->	
+	</section><!--especialidades-->
+
+	<section class="extras">
+		<div class ="center">
+			<div id="depoimentos" class="w50 left depoimentos-container">
+				<h2 class="title">Depoimentos dos nossos clientes</h2>
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien." </p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->	
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien." </p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->	
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien." </p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->	
+				<div class="depoimento-single">
+					<p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien."</p>
+					<p class="nome-autor">Lorem Ipsum</p>
+				</div><!--depoimento-single-->	
+			</div><!--w50-->
+				<div id="servicos" class="w50 left servicos-container">
+				<h2 class="title">Serviços</h2>
+				<div class="servicos">
+				<ul> 
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+					<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus bibendum suscipit scelerisque. Nunc vitae erat nec nisi blandit pulvinar. Morbi convallis non neque et placerat. Aenean et posuere sapien. Etiam luctus, elit in vulputate rutrum, mauris diam volutpat justo, in tincidunt neque nisi ac velit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+				</ul>
+			</div><!--servicos-->
+			</div><!--w50-->
+			
+			<div class="clear"></div>
+		</div><!--center-->	
+	</section><!--extras-->
